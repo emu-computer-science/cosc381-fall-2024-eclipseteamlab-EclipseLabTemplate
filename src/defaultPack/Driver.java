f459e8ab50d926caf86066eeba09028fd6a97e04
Hope Heck <113306534+tinymightyhopester@users.noreply.github.com>
2024-09-30 14:17:54 -0400

    hungry class

--- src/defaultPack/Driver.java
+++ src/defaultPack/Driver.java
@@ -5,6 +5,8 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Our team is getting started");
+		Hungry h = new Hungry();
+		h.hungry();
 
 	}
 
---
+++ src/defaultPack/Hungry.java
@@ -0,0 +1,7 @@
+package defaultPack;
+
+public class Hungry {
+	public static void hungry() {
+		System.out.println("We are hungry");
+	}
+}
