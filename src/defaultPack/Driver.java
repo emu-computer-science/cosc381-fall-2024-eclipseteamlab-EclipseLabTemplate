455891becf530a3b36375c321f7fff6e05174c59
jhotchk2 <jhotchk2@emich.edu>
2024-09-30 14:13:21 -0400

    added Driver.java file

---
+++ src/defaultPack/Driver.java
@@ -0,0 +1,11 @@
+package defaultPack;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Our team is getting started");
+
+	}
+
+}
